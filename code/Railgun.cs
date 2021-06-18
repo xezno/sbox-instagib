c9cfb1b41528f3fa10744847dafd413dd9a2bfcd
Alex <alex@gu3.me>
2021-06-18 16:39:02 +0100

    Improve hit tolerance Replace exceptions with logs for debugging

--- code/Railgun.cs
+++ code/Railgun.cs
@@ -10,10 +10,11 @@ namespace Instagib
 		public override string ViewModelPath => "weapons/railgun/models/wpn_qc_railgun.vmdl";
 		public override float PrimaryRate => 1 / 1.5f;
 
+		private const float maxHitTolerance = (90f / 1000f) * 500; // (tickrate / 1000ms) * desired_ms, number of ticks tolerance given to a hit. 
 		private float zoomFov = 60f;
 
 		private Particles beamParticles;
-		private bool zoom;
+		private bool isZooming;
 
 		public override void Reload() { } // No reload
 
@@ -48,44 +49,88 @@ namespace Instagib
 			Shoot( Owner.EyePos, Owner.EyeRot.Forward );
 		}
 
+		// This should totally not be a user-invoked command
 		[ServerCmd( "send_shoot" )]
-		private static void CmdShoot( int targetIdent, int ownerIdent, Vector3 endPos, Vector3 forward, int tick )
+		private static void CmdShoot( int targetIdent, int ownerIdent, Vector3 startPos, Vector3 endPos, Vector3 forward, int tick )
 		{
-			// This should totally not be a command
+			Host.AssertServer();
+
+			var target = Entity.All.First( e => e.NetworkIdent == targetIdent );
+			var owner = Entity.All.First( e => e.NetworkIdent == ownerIdent );
+
+			//
+			// Checking:
 			// In order to prevent people from just typing stuff like "shoot Alex", we'll do some light checking to
 			// verify stuff
-
-			try
+			//	
+			if ( target is not InstagibPlayer )
 			{
-				var target = Entity.All.First( e => e.NetworkIdent == targetIdent );
-				var owner = Entity.All.First( e => e.NetworkIdent == ownerIdent );
-				var damage = DamageInfo.FromBullet( endPos, forward.Normal * 20, 1000 )
-					.WithAttacker( owner ).WithWeapon( new Railgun() ).WithForce( (endPos - forward).Normal * 1000f );
+				// Fail silently - player probably missed their shot?
+				Log.Trace( "Target wasn't a player" );
+				return;
+			}
 
-				//if ( target is not Player )
-				//	throw new Exception();
+			if ( owner is not Player )
+			{
+				// This should never happen 
+				Log.Trace( "Owner wasn't a player" );
+				return;
+			}
 
-				//if ( owner is not Player )
-				//	throw new Exception();
+			if ( tick - Time.Tick > maxHitTolerance ) // This is equivalent to
+			{
+				Log.Trace( $"Too much time passed: {tick - Time.Tick}" );
+				return; // Too much time passed - player's lagging too much for us to do any proper checks
+			}
 
-				if ( Time.Tick - tick > 64 )
+			// TODO: Check to make sure that the end pos and forward actually line up with the start pos
+			{
+
+			}
+
+			// TODO: Do some basic checking to ensure that this isn't called more times/sec than it should be
+			{
+
+			}
+
+			// Do a (large) raycast in the direction specified to make sure they're not bullshitting
+			{
+				var tr = Trace.Ray( startPos, startPos + forward * 100000 )
+						.UseHitboxes()
+						.Ignore( owner )
+						.Size( 25f ) // This determines the tolerance of the cast; 25 is a good value for most playable pings
+						.EntitiesOnly()
+						.Run();
+
+				if ( !tr.Hit )
+				{
+					Log.Trace( "Didn't hit" );
 					return;
+				}
 
-				// TODO - Checking:
-				// Check to make sure that the end pos and forward actually line up with the start pos
-				// Do some basic checking to ensure that this isn't called more times/sec than it should be
-				// Do a (large) raycast in the direction specified to make sure they're not fucking us with aim
-				target.TakeDamage( damage );
-			}
-			catch
-			{
-				Log.Error( $"Invalid target" );
+				if ( !tr.Entity.IsValid() )
+				{
+					Log.Trace( "Entity invalid" );
+					return;
+				}
 
-				ConsoleSystem.Run( "kick", ConsoleSystem.Caller.Name ); // :)
+				if ( tr.Entity.NetworkIdent != targetIdent )
+				{
+					Log.Trace( "Idents didnt match" );
+					return;
+				}
 			}
+
+			//
+			// Damage
+			//
+			var damage = DamageInfo.FromBullet( endPos, forward.Normal * 20, 1000 )
+				.WithAttacker( owner ).WithWeapon( new Railgun() );
+
+			target.TakeDamage( damage );
 		}
 
-		[ClientRpc()]
+		[ClientRpc]
 		private void Shoot( Vector3 pos, Vector3 dir )
 		{
 			var forward = dir * 10000;
@@ -101,8 +146,8 @@ namespace Instagib
 
 				if ( !tr.Entity.IsValid() ) continue;
 
-				// TODO: We shouldn't be running this as a command!!!!
-				ConsoleSystem.Run( "send_shoot", tr.Entity.NetworkIdent, Owner.NetworkIdent, tr.EndPos, Owner.EyeRot.Forward, Time.Tick );
+				// This is the only way to do client->server RPCs :(
+				ConsoleSystem.Run( "send_shoot", tr.Entity.NetworkIdent, Owner.NetworkIdent, pos, tr.EndPos, dir, Time.Tick );
 			}
 
 			ShootEffects();
@@ -118,14 +163,14 @@ namespace Instagib
 				beamParticles.SetPos( 1, tr.EndPos );
 			}
 
-			zoom = Input.Down( InputButton.View );
+			isZooming = Input.Down( InputButton.View );
 		}
 
 		public override void PostCameraSetup( ref CameraSetup camSetup )
 		{
 			base.PostCameraSetup( ref camSetup );
 
-			if ( zoom )
+			if ( isZooming )
 			{
 				camSetup.FieldOfView = zoomFov;
 			}
@@ -133,7 +178,7 @@ namespace Instagib
 		
 		public override void BuildInput( InputBuilder owner ) 
 		{
-			if ( zoom )
+			if ( isZooming )
 			{
 				// Half input sensitivity
 				owner.ViewAngles = Angles.Lerp( owner.OriginalViewAngles, owner.ViewAngles, zoomFov / 90f );
