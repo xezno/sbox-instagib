967db57b76e5d7fac6c503eaf41648b01760d1aa
Alex <alex@gu3.me>
2021-06-19 14:49:46 +0100

    Remove directional arrows

--- code/UI/Crosshair.cs
+++ code/UI/Crosshair.cs
@@ -9,37 +9,14 @@ namespace Instagib.UI
 	{
 		private static Crosshair Instance { get; set; }
 		
-		private Panel up;
-		private Panel left;
-		private Panel right;
-		private Panel down;
-		
 		public Crosshair()
 		{
 			Instance = this;
 			
 			SetClass( "crosshair", true );
 			SetText( "a" );
-
-			var crosshairMove = AddChild<Panel>( "crosshair-move" );
-			
-			up = crosshairMove.AddChild<Panel>( "up" );
-			left = crosshairMove.AddChild<Panel>( "left" );
-			right = crosshairMove.AddChild<Panel>( "right" );
-			down = crosshairMove.AddChild<Panel>( "down" );
 			
 			StyleSheet.Load( "/Code/UI/InstagibHud.scss" );
-		}
-
-		[Event.BuildInput]
-		public void BuildInput( InputBuilder input )
-		{
-			Host.AssertClient();
-
-			up.SetClass( "active", input.Down( InputButton.Forward ) );
-			left.SetClass( "active", input.Down( InputButton.Left ) );
-			right.SetClass( "active", input.Down( InputButton.Right ) );
-			down.SetClass( "active", input.Down( InputButton.Back ) );
 		}
 
 		[ClientCmd( "crosshair_glyph" )]
