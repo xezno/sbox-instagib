72b44874b360092e3eae8e21f835a7cb1f865a18
Alex <alex@gu3.me>
2021-06-24 02:57:55 +0100

    Base color picker UI

---
+++ code/UI/Elements/ColorPicker.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Instagib.Utils;
+using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+namespace Instagib.UI.Elements
+{
+	public class ColorPicker : Panel
+	{
+		private class RgbInput : Panel
+		{
+			private TextEntry rEntry;
+			private TextEntry gEntry;
+			private TextEntry bEntry;
+			
+			public RgbInput()
+			{
+				var rPanel = Add.Panel();
+				rPanel.Add.Label( "R" );
+				rEntry = rPanel.Add.TextEntry( "0" );
+				
+				var gPanel = Add.Panel();
+				gPanel.Add.Label( "G" );
+				gEntry = gPanel.Add.TextEntry( "0" );
+				
+				var bPanel = Add.Panel();
+				bPanel.Add.Label( "B" );
+				bEntry = bPanel.Add.TextEntry( "0" );
+			}
+
+			public void SetValues( Color color )
+			{
+				rEntry.Text = (color.r).ToString("F0");
+				gEntry.Text = (color.g).ToString("F0");
+				bEntry.Text = (color.b).ToString("F0");
+			}
+		}
+
+		private byte[] imageData;
+		private int width = 360;
+		private int height = 150;
+		private int stride = 4;
+
+		private Slider valueSlider;
+		private RgbInput input;
+		private Image image;
+		
+		public ColorPicker()
+		{
+			StyleSheet.Load( "/Code/UI/Elements/ColorPicker.scss" );
+			
+			image = Add.Image();
+			input = AddChild<RgbInput>();
+			
+			DoTextureStuff();
+				
+			var valPanel = Add.Panel();
+			valPanel.Add.Label( "Value" );
+			valueSlider = valPanel.AddChild<Slider>();
+			valueSlider.SnapRate = 5;
+			valueSlider.Value = 1.0f;
+			valueSlider.OnValueChange += value =>
+			{
+				DoTextureStuff( value );
+			};
+		}
+
+		private void DoTextureStuff( int value = 0 )
+		{
+			float fValue = value / 100f;
+			
+			var pixelColor = Color.Red;
+			var hslColor = HSV.ColorToHSV( pixelColor );
+
+			hslColor.value = fValue;
+
+			var data = new byte[width * height * stride];
+			imageData = data;
+
+			for ( int i = 0; i < data.Length; ++i )
+				data[i] = 255;
+
+			void SetPixel( int x, int y, Color col )
+			{ 
+				byte ColToByte( float v ) => (byte)MathF.Floor( (v >= 1.0f) ? 255f : v * 256.0f );
+
+				data[((x + (y * width)) * stride) + 0] = ColToByte( col.r );
+				data[((x + (y * width)) * stride) + 1] = ColToByte( col.g );
+				data[((x + (y * width)) * stride) + 2] = ColToByte( col.b );
+				data[((x + (y * width)) * stride) + 3] = 255;
+			}
+
+			for (int y = 0; y < height; y++)
+			{
+				hslColor.hue = 0;
+				
+				for (int x = 0; x < width; x++)
+				{
+					var hsvConvert = hslColor.HSVToColor();
+					pixelColor = hsvConvert;
+					SetPixel( x, y, pixelColor );
+					hslColor.hue += 1;
+				}
+				hslColor.saturation -= (y * 0.0001f); // 40 works, don't change it
+			}
+
+			var texture = Texture.Create( width, height ).WithStaticUsage().WithData( data ).WithName( "hsvColor" ).Finish();
+
+			image.Texture = texture;
+		}
+
+		protected override void OnClick( MousePanelEvent e )
+		{
+			base.OnClick( e );
+			
+			Color GetPixel( int x, int y )
+			{
+				var col = new Color
+				{
+					r = imageData[((x + (y * width)) * stride) + 0],
+					g = imageData[((x + (y * width)) * stride) + 1],
+					b = imageData[((x + (y * width)) * stride) + 2],
+					a = imageData[((x + (y * width)) * stride) + 3]
+				};
+
+				return col;
+			}
+
+			var localPos = Mouse.Position - image.Box.Rect.Position;
+			if ( localPos.Outside( Vector2.Zero ) || localPos.Inside( image.Box.Rect.Size ) )
+				return;
+			
+			var normalizedPos = localPos / image.Box.Rect.Size;
+
+			var arrayPos = normalizedPos * new Vector2( width, height );
+			
+			var arrayEntry = GetPixel( (int)arrayPos.x, (int)arrayPos.y );
+
+			input.SetValues( arrayEntry );
+		}
+	}
+}
---
+++ code/Utils/HSV.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Sandbox;
+
+namespace Instagib.Utils
+{
+	public static class HSV
+	{
+		public struct HSVColor
+		{
+			public float hue;
+			public float saturation;
+			public float value;
+			
+			public HSVColor( float hue, float saturation, float value )
+			{
+				this.hue = hue;
+				this.saturation = saturation;
+				this.value = value;
+			}
+		}
+		
+		public static Color HSVToColor( this HSVColor hsv )
+		{
+			var hi = Convert.ToInt32( MathF.Floor( hsv.hue / 60.0f ) ) % 6;
+			var f = hsv.hue / 60.0f - MathF.Floor( hsv.hue / 60.0f );
+
+			var v = hsv.value;
+			var p = hsv.value * (1 - hsv.saturation);
+			var q = hsv.value * (1 - f * hsv.saturation);
+			var t = hsv.value * (1 - (1 - f) * hsv.saturation);
+
+			return hi switch
+			{
+				0 => new Color( v, t, p ),
+				1 => new Color( q, v, p ),
+				2 => new Color( p, v, t ),
+				3 => new Color( p, q, v ),
+				4 => new Color( t, p, v ),
+				_ => new Color( v, p, q )
+			};
+		}
+
+		private static float CalcHue( this Color col )
+		{
+			float min = Math.Min( Math.Min( col.r, col.g ), col.b );
+			float max = Math.Max( Math.Max( col.r, col.g ), col.b );
+
+			if ( min.AlmostEqual( max ) ) return 0;
+
+			float hue;
+			if ( max.AlmostEqual( col.r ) )
+				hue = (col.g - col.b) / (max - min);
+			else if ( max.AlmostEqual( col.g ) )
+				hue = 2f + (col.b - col.r) / (max - min);
+			else
+				hue = 4f + (col.r - col.g) / (max - min);
+
+			hue *= 60;
+			if ( hue < 0 ) hue += 360;
+
+			return MathF.Round( hue );
+		}
+		
+		public static HSVColor ColorToHSV( this Color color )
+		{
+			float max = MathF.Max( color.r, Math.Max( color.g, color.b ) );
+			float min = MathF.Min( color.r, Math.Min( color.g, color.b ) );
+
+			float hue = color.CalcHue();
+			float saturation = (max == 0) ? 0 : 1f - (1f * min / max);
+			float value = max / 255f;
+
+			return new HSVColor( hue, saturation, value );
+		}
+	}
+}
